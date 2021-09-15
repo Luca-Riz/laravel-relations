91473becbfc312f84fe7dbc8a4152cc27ee7bfb8
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-09-15 14:04:59 +0200

    init

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //validazione dei dati
+        $request->validate([
+            'title' => 'required|max:60',
+            'content' => 'required'
+        ]);
+
+        // prendere dati
+        $data = $request->all();
+
+        // creare nuova istanza coi dati ottenuti dalla request
+        $new_post = new Post();
+
+        $slug = Str::slug($data['title'], '-');
+
+        // se c'é un duplicato
+        $slug_base = $slug;
+
+        $slug_presente = Post::where('slug', $slug)->first();
+
+        $contatore = 1;
+        while ($slug_presente) {
+            $slug = $slug_base . '-' . $contatore;
+
+            $slug_presente = Post::where('slug', $slug)->first();
+
+            $contatore++;
+        }
+        // end se c'é un duplicato
+
+        $new_post->slug = $slug;
+        $new_post->fill($data);
+
+        // salvare
+        $new_post->save();
+
+        return redirect()->route('admin.posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    //collegamento con slug (utilizzato nel front office)
+    public function show($slug)
+    {
+        $post = Post::where('slug',$slug)->first();
+        return view('admin.posts.show', compact('post'));
+    }
+
+    //collegamento con id
+    // public function show(Post $post)
+    // {
+    //     return view('admin.posts.show', compact('post'));
+    // }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return view('admin.posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $data = $request->all();
+
+        //? se lo slug é diverso dal precedente ricalcolalo
+        if($data['title'] != $post->title){
+
+            $slug = Str::slug($data['title'], '-'); //titolo-di-esempio
+            $slug_base = $slug; //titolo-di-esempio
+
+            // se lo slug é uguale ad uno giá presente
+            $slug_presente = Post::where('slug', $slug)->first();
+
+            $contatore = 1;
+            while($slug_presente){
+
+                //aggiungiamo al post di prima il -contatore
+                $slug = $slug_base . '-' . $contatore; //titolo-di-esempio-1
+
+                //controlliamo se il post esiste ancora
+                $slug_presente = Post::where('slug', $slug)->first();
+
+                //incrementiamo il contatore
+                $contatore++;
+            }
+
+            //in ogni caso assegniamo allo slug il valore ottenuto
+            $data['slug'] = $slug;
+        }
+
+        $post->update($data);
+
+        return redirect()->route('admin.posts.index')->with('updated', 'Hai modificato con successo l\'elemento ' . $post->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect()->route('admin.posts.index');
+    }
+}
---
+++ resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+          @foreach ($errors->all() as $error)
+              <li>{{$error}}</li>
+          @endforeach
+        </ul>
+      </div>
+        
+    @endif
+
+    <div class="container">
+      <form action="{{route('admin.posts.update', $post->id)}}" method="post"> {{-- method non é indispensabile --}}
+        @csrf
+        @method('PATCH')
+        <div class="mb-3">
+          <label for="titolo" class="form-label">Titolo</label>
+          <input type="text" class="form-control" id="titolo" name='title' value="{{ old('title', $post->title)}}">
+        </div>
+  
+        <div class="mb-3">
+          <label for="desc" class="form-label">Descrizione</label>
+          <textarea class="form-control" name="content" id="desc" cols="30" rows="10">{{old('content', $post->content)}}</textarea>
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+
+  </div>
+    
+@endsection
