163987d9760d2251c6ad8487e6660933c811251b
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-09-15 15:20:29 +0200

    add connection between posts and categories tables

--- app/Post.php
+++ app/Post.php
@@ -9,6 +9,11 @@ class Post extends Model
     protected $fillable = [
         'title',
         'slug',
-        'content'
+        'content',
+        'category_id' //? aggiunta questa riga dopo migrations/update_posts_table
     ];
+
+    public function postCategory(){
+        return $this->hasMany('App\Category');
+    }
 }
